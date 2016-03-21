6f648ac4aca90497af37289c93b99142d7fb8093
dlinch <dlinch33@gmail.com>
2016-03-20 23:55:23 -0600

    fixed data model, migration, and added user seed data

---
+++ migrations/20160320233621_new.js
@@ -0,0 +1,37 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table){
+    table.increments().primary();
+    table.string('first_name');
+    table.string('last_name');
+    table.string('email');
+    table.string('password');
+    table.string('picture_url');
+    table.boolean('shoveler');
+    table.string('home_address');
+  }).then(function(){
+    return knex.schema.createTable('shovelers', function(table){
+      table.increments().primary();
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('cascade')
+    }).then(function(){
+      return knex.schema.createTable('jobs', function(table){
+        table.increments().primary();
+        table.string('time');
+        table.integer('requester_id').unsigned().references('id').inTable('users').onDelete('cascade');
+        table.integer('shoveler_id').unsigned().references('id').inTable('shovelers').onDelete('cascade');
+        table.boolean('complete');
+        table.string('type');
+        table.integer('zipcode');
+        table.string('address');
+      })
+    })
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('shovelers').then(function(){
+    return knex.schema.dropTable('jobs').then(function(){
+      return knex.schema.dropTable('users');
+    })
+  })
+};
--- seeds/testData.js
+++ seeds/testData.js
@@ -1,31 +1,37 @@
-var knex = require('../db/knex');
+ var knex = require('../db/knex');
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('users').del().then(function(){
-    // Inserts seed entries
-    return Promise.all([
-      knex('users').insert({
-        first_name: 'Derik',
-        last_name: 'Linch',
-        email: 'dlinch33@gmail.com',
-        pro: false,
-        token: 'password1'
-        }),
-      knex('users').insert({
-        first_name: 'Ashley',
-        last_name: 'Lauren',
-        email: 'dlinch33@gmail.com',
-        pro: true,
-        token: 'password2'
-        }),
-      knex('users').insert({
-        first_name: 'Bryce',
-        last_name: 'Linch',
-        email: 'bmlinch@gmail.com',
-        pro: false,
-        token: 'password3'
-      }),
-    ])
-  })
- };
+ exports.seed = function(knex, Promise) {
+   // Deletes ALL existing entries
+   return knex('users').del().then(function(){
+     // Inserts seed entries
+     return Promise.all([
+       knex('users').insert({
+         first_name: 'Derik',
+         last_name: 'Linch',
+         email: 'dlinch33@gmail.com',
+         password: 'password',
+         picture_url: 'www.google.com/',
+         shoveler: true,
+         home_address: '8570 West 95th Drive Westminster CO 80021'
+         }),
+         knex('users').insert({
+           first_name: 'Bryce',
+           last_name: 'Linch',
+           email: 'bmlinch@gmail.com',
+           password: 'password',
+           picture_url: 'www.google.com/',
+           shoveler: false,
+           home_address: '8570 West 95th Drive Westminster CO 80021'
+           }),
+           knex('users').insert({
+             first_name: 'David',
+             last_name: 'Shibley',
+             email: 'shibbles@gmail.com',
+             password: 'password',
+             picture_url: 'www.google.com/',
+             shoveler: true,
+             home_address: '1644 Platte Street Denver CO'
+             }),
+     ])
+   })
+  };
