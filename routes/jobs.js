d0b35e0dfafea44cb59dc406c0a1cfb9d475b68b
dlinch <dlinch33@gmail.com>
2016-03-22 15:25:17 -0600

    geolocation filtering with geonames api now works

--- routes/jobs.js
+++ routes/jobs.js
@@ -9,82 +9,92 @@ var Jobs = function() {
 }
 
 var geocodeData;
-function areaCodeFilter(Jobject){
-  for(i=0; i<data.geocodeData.body.postalCodes.length; i++){
-    for(j=0; j<Jobject.length; j++){
-      if(Jobject[i]==geocodeData.body.postalCodes[i].postalCode){
-        console.log('filter true')
-        return true;
-      }
-    }
-    console.log('filter false')
-    return false;
-  }
-}
 
-function requestPostalCodes(radius, postalcode){
-  if(!postalcode){
+
+function requestPostalCodes(radius, postalcode) {
+  if (!postalcode) {
     postalcode = 80205;
   }
-  return new Promise(function(resolve, reject){
-    unirest.get('http://api.geonames.org/findNearbyPostalCodesJSON?postalcode='+postalcode+'&country=US&radius='+radius+'&username='+process.env.GEONAME_USERNAME)
-    .end(function(response){
-      resolve(response);
-    })
+  return new Promise(function(resolve, reject) {
+    unirest.get('http://api.geonames.org/findNearbyPostalCodesJSON?postalcode=' + postalcode + '&country=US&radius=' + radius + '&username=' + process.env.GEONAME_USERNAME)
+      .end(function(response) {
+        resolve(response);
+      })
   })
 }
 
-router.get('/geocode/:radius', function(req, res, next){
-
-})
+// router.get('/geocode/:radius', function(req, res, next){
+//
+// })
 
 // All jobs X
-router.get('/', function(req, res, next){
-  Jobs().select().then(function(data){
+router.get('/', function(req, res, next) {
+  Jobs().select().then(function(data) {
     res.json(data)
   })
 })
 
 // See available jobs. X
 router.get('/available/:userID/:radius/:zipcode', function(req, res, next) {
-  Jobs().whereNot(function(){
-    this.whereNotNull('shoveler_id').orWhere({requester_id: req.params.userID, complete: true})
-  }).select().then(function(data){
-    console.log(data)
-    geocodeData = requestPostalCodes(req.params.radius, req.params.zipcode)
-    console.log(geocodeData)
-    return data;
-      })
-    .then(function(data){
-    var returnArray = data.filter(areaCodeFilter)
-    res.json(returnArray);
-  }).catch(function(error){
+  Jobs().whereNot(function() {
+    this.whereNotNull('shoveler_id').orWhere({
+      requester_id: req.params.userID,
+      complete: true
+    })
+  }).select().then(function(db) {
+    // console.log(db)
+    requestPostalCodes(req.params.radius, req.params.zipcode).then(function(data) {
+      // console.log(data)
+      geocodeData = data.body.postalCodes;
+      console.log(geocodeData.length);
+
+
+      function areaCodeFilter(Jobject) {
+        console.log('geocode log')
+        for (var i = 0; i < geocodeData.length; i++) {
+          console.log(i)
+          if (Jobject.zipcode == geocodeData[i].postalCode) {
+            return true;
+          }
+        }
+        return false;
+      }
+
+
+
+
+
+      var returnArray = db.filter(areaCodeFilter)
+      console.log(returnArray)
+      res.json(returnArray);
+    })
+  }).catch(function(error) {
     res.json(error)
   })
 });
 
 // See all jobs that you have shoveled.
-router.get('/shoveledjobs/:userID', function(req, res, next){
+router.get('/shoveledjobs/:userID', function(req, res, next) {
   Jobs().where({
-    shoveler_id: req.params.userID,
-    complete: true
-  }).select()
-  .then(function(data){
-    res.json(data);
-  }).catch(function(error){
-    res.json(error)
-  })
+      shoveler_id: req.params.userID,
+      complete: true
+    }).select()
+    .then(function(data) {
+      res.json(data);
+    }).catch(function(error) {
+      res.json(error)
+    })
 })
 
 // See all jobs you have accepted X
-router.get('/currentjobs/:userID', function(req, res, next){
+router.get('/currentjobs/:userID', function(req, res, next) {
   Jobs().where({
-    shoveler_id: req.params.userID,
-    complete: false
-  }).select()
-  .then(function(data){
-    res.json(data)
-  })
+      shoveler_id: req.params.userID,
+      complete: false
+    }).select()
+    .then(function(data) {
+      res.json(data)
+    })
 });
 
 // See all current jobs a user has posted. X
@@ -92,9 +102,9 @@ router.get('/myjobs/:userID', function(req, res, next) {
   Jobs().where({
     requester_id: req.params.userID,
     complete: false
-}).select().then(function(data){
+  }).select().then(function(data) {
     res.json(data);
-  }).catch(function(error){
+  }).catch(function(error) {
     res.json(error)
   })
 });
@@ -102,7 +112,7 @@ router.get('/myjobs/:userID', function(req, res, next) {
 // See
 
 // Post a new job. X
-router.post('/new/:id', function(req, res, next){
+router.post('/new/:id', function(req, res, next) {
   Jobs().insert({
     requester_id: knex('users').where('id', req.params.id).select('id'),
     shoveler_id: null,
@@ -111,72 +121,72 @@ router.post('/new/:id', function(req, res, next){
     zipcode: req.body.zipcode,
     address: req.body.address,
     time: Date(),
-  }, 'id').then(function(data){
+  }, 'id').then(function(data) {
     res.json(data);
   })
 })
 
 
 // Update a current job. X
-router.put('/update/:jobID', function(req, res, next){
+router.put('/update/:jobID', function(req, res, next) {
   console.log(req.body.zipcode, req.body.address, req.body.property)
   Jobs().where('id', req.params.jobID).update({
     time: Date(),
     zipcode: req.body.zipcode,
     address: req.body.address,
     type: req.body.property,
-  }).then(function(data){
+  }).then(function(data) {
     res.json(data)
-  }).catch(function(error){
+  }).catch(function(error) {
     res.json(error)
   })
 })
 
 
 // Mark a job as complete. X
-router.put('/complete/:jobID', function(req, res, next){
+router.put('/complete/:jobID', function(req, res, next) {
   Jobs().where('id', req.params.jobID).update({
     complete: true
-  }).then(function(data){
+  }).then(function(data) {
     res.json(data)
-  }).catch(function(error){
+  }).catch(function(error) {
     res.json(error)
   })
 })
 
 // Accept a job. X
-router.put('/accept/:jobID/:userID', function(req, res, next){
+router.put('/accept/:jobID/:userID', function(req, res, next) {
   Jobs().where('id', req.params.jobID).update({
     shoveler_id: req.params.userID
-  }).then(function(data){
+  }).then(function(data) {
     res.json(data)
-  }).catch(function(error){
+  }).catch(function(error) {
     res.json(error)
   })
 })
 
 // Unaccept a job X
-router.put('/unaccept/:jobID', function(req, res, next){
+router.put('/unaccept/:jobID', function(req, res, next) {
   Jobs().where('id', req.params.jobID).update({
     shoveler_id: null
-  }).then(function(data){
+  }).then(function(data) {
     res.json(data)
-  }).catch(function(error){
+  }).catch(function(error) {
     res.json(error)
   })
 })
 
 // Cancel a current job.
-router.delete('/delete/:jobID', function(req, res, next){
+router.delete('/delete/:jobID', function(req, res, next) {
   Jobs().where({
-    id: req.params.jobID,
-    complete: false
-  }).del()
-  .then(function(data){
-    res.json(data)
-  }).catch(function(error){
-    res.json(error)
-  })
+      id: req.params.jobID,
+      complete: false
+    }).del()
+    .then(function(data) {
+      res.json(data)
+    }).catch(function(error) {
+      res.json(error)
+    })
 })
 
 
