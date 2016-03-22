e46fee4218498b2766c77d01d41ce7ca5c2a6c4b
dlinch <dlinch33@gmail.com>
2016-03-22 17:58:48 -0600

    fixed crashing stripe again

--- routes/stripe.js
+++ routes/stripe.js
@@ -6,16 +6,17 @@ var stripe = require('stripe')(process.env.STRIPE_KEY)
 
 
 router.get('/', function(req, res, next) {
-  var stripeToken= req.body.stripeToken;
-  var charge = stripe.charges.create({
-  amount: 1000, // amount in cents, again
-  currency: "usd",
-  source: stripeToken,
-  description: "Example Shoveling"
-}, function(err, charge){
-  if(err && err.type === 'StripeCardError'){
-    //Card declined.
-  }
-});
-
-module.exports = router;
+      var stripeToken = req.body.stripeToken;
+      var charge = stripe.charges.create({
+        amount: 1000, // amount in cents, again
+        currency: "usd",
+        source: stripeToken,
+        description: "Example Shoveling"
+      }, function(err, charge) {
+        if (err && err.type === 'StripeCardError') {
+          //Card declined.
+        }
+        res.json('YAY');
+      });
+    });
+    module.exports = router;
