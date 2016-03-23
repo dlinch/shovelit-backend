1696732a248c6b98e6031620a5311e36d381a8b2
dlinch <dlinch33@gmail.com>
2016-03-22 23:05:02 -0600

    stripe testing to update db

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,23 +1,38 @@
 var express = require('express');
 var router = express.Router();
 require('dotenv').load();
+var knex = require('../db/knex');
+
 
 var stripe = require('stripe')(process.env.STRIPE_KEY)
 
+var Jobs = function() {
+  return knex('jobs')
+}
 
-router.post('/', function(req, res, next) {
-      var stripeToken = req.body.stripeToken;
-      var charge = stripe.charges.create({
-        amount: 1000, // amount in cents, again
-        currency: "usd",
-        source: stripeToken,
-        description: "Example Shoveling"
-      }, function(err, charge) {
-        if (err && err.type === 'StripeCardError') {
-          //Card declined.
-          res.json(error);
-        }
-        res.redirect('https://skyffel.co/#/dashboard');
-      });
+
+router.post('/:jobID', function(req, res, next) {
+  Jobs().where('id', req.params.jobID)
+  .update({
+    paid: true
+  }).then(function(){
+    var stripeToken = req.body.stripeToken;
+    var charge = stripe.charges.create({
+      amount: 1000, // amount in cents, again
+      currency: "usd",
+      source: stripeToken,
+      description: "Example Shoveling"
+    }, function(err, charge) {
+      if (err && err.type === 'StripeCardError') {
+        //Card declined.
+        res.json(error);
+      }
+      res.redirect('https://skyffel.co/#/dashboard');
     });
-    module.exports = router;
+
+
+
+  })
+
+});
+module.exports = router;
